5a923f31a762e40ec2b3531579265b7d37bfeefd
raj1020 <raj.1020@hotmail.com>
2019-10-14 15:57:59 +1100

    made corrections in the loops so that they deliver expected results

--- positive_dominant.rb
+++ positive_dominant.rb
@@ -1,17 +1,23 @@
 def positive_dominant(arr)
     #Your code here
     i=0
+    a=0
+    b=0
     positives=0
     negatives=0
-    while i<arr.count
+    
+    while i<(arr.count)
         j=i+1
         while j<arr.count
-            if arr[i]=arr[j]
+            if arr[i]==arr[j]
                 arr.delete_at(j)
                 next
             end
+            p "j=#{j}"
             j+=1
+           
         end
+
         if arr[i]>0
             positives+=1
         end
@@ -19,8 +25,12 @@ def positive_dominant(arr)
         if arr[i]<0
             negatives+=1
         end
+        p "i=#{i}"
         i+=1
+       
     end
+
+   
 p positives
 p negatives
 end
---
+++ test.rb
@@ -0,0 +1,12 @@
+def positive_dominant(arr)
+    #Your code here
+    p arr.shift
+    i=0
+    j=i+1
+   p arr
+end
+
+ positive_dominant([5, 99, -3, -8, 832, -3, -8])
+#p positive_dominant([2, 2, 2, -5, 2, 2, 2, -3])
+#p positive_dominant([2, 4, 6, -6, -4, -2])
+#p positive_dominant(["String", 34, true])
