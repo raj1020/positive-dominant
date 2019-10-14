0af02a7b056881a1029e024db9f447d81fe5c7e9
raj1020 <raj.1020@hotmail.com>
2019-10-14 16:39:15 +1100

    added comments and added question at th e top of the code

--- positive_dominant.rb
+++ positive_dominant.rb
@@ -1,10 +1,19 @@
+#An Array is positive dominant if it contains more unique positive numbers than unique 
+#negative numbers. Write a method that will take an array as it's argument, and will 
+#return true if the array is positive dominant, or false if it is not. Note, you may 
+#receive some other data types in the arrays. These should be ignored.
+# E.g. positive_dominant([5, 99, -3, -8, 832, -3, -8]), should return true (as there are 3 unique positive numbers, and only 2 unique negative numbers)
+# E.g. positive_dominant([2, 2, 2, -5, 2, 2, 2, -3]), should return false
+# E.g. positive_dominant([2, 4, 6, -6, -4, -2]), should return false
+# E.g. positive_dominant(["String", 34, true]), should return true
+
 def positive_dominant(arr)
-    #Your code here
+    
     i=0
     a=0
     positives=0
     negatives=0
-    while a<arr.count
+    while a<arr.count #Used the while loop to get rid of the elements other than floats or integers in the array
         if (arr[a].is_a?(Integer) || arr[a].is_a?(Float))
              
              a+=1
@@ -14,7 +23,7 @@ def positive_dominant(arr)
      
      end
     
-    while i<(arr.count)
+    while i<(arr.count) #THis while loop counts the number of positive and negative elements in the array
         j=i+1
         while j<arr.count
             if arr[i]==arr[j]
@@ -39,11 +48,11 @@ def positive_dominant(arr)
     end
 
    
-if positives > negatives || arr==[0]
-    return true
-else
-    return false
-end
+    if positives > negatives || arr==[0]
+        return true
+    else
+        return false
+    end
 
 end
 
