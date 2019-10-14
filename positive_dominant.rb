5743a2b27de452d1c16d8de305558e563dd650ba
raj1020 <raj.1020@hotmail.com>
2019-10-14 16:31:56 +1100

    changed the code so that it returns the results as desired

--- positive_dominant.rb
+++ positive_dominant.rb
@@ -5,7 +5,7 @@ def positive_dominant(arr)
     positives=0
     negatives=0
     while a<arr.count
-        if (arr[a].is_a?(Integer) || arr[a].is_a?(Float))== true
+        if (arr[a].is_a?(Integer) || arr[a].is_a?(Float))
              
              a+=1
         else
@@ -15,9 +15,6 @@ def positive_dominant(arr)
      end
     
     while i<(arr.count)
-        
-
-
         j=i+1
         while j<arr.count
             if arr[i]==arr[j]
@@ -42,11 +39,15 @@ def positive_dominant(arr)
     end
 
    
-p positives
-p negatives
+if positives > negatives || arr==[0]
+    return true
+else
+    return false
 end
 
- positive_dominant([5, 99, -3, -8, 832, -3, -8])
- positive_dominant([2, 2, 2, -5, 2, 2, 2, -3])
- positive_dominant([2, 4, 6, -6, -4, -2])
- positive_dominant(["String", 34, true])
+end
+
+ p positive_dominant([5, 99, -3, -8, 832, -3, -8])
+ p positive_dominant([2, 2, 2, -5, 2, 2, 2, -3])
+ p positive_dominant([2, 4, 6, -6, -4, -2])
+ p positive_dominant(["String", 34, true])
