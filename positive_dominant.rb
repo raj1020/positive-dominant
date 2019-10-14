2e5e3ab070849f90830bb23516fcb18e3075266c
raj1020 <raj.1020@hotmail.com>
2019-10-14 13:55:03 +1100

    initial commit; framework of method and code to call methods

---
+++ positive_dominant.rb
@@ -0,0 +1,10 @@
+def positive_dominant(arr)
+    #Your code here
+    
+
+end
+
+p positive_dominant([5, 99, -3, -8, 832, -3, -8])
+p positive_dominant([2, 2, 2, -5, 2, 2, 2, -3])
+p positive_dominant([2, 4, 6, -6, -4, -2])
+p positive_dominant(["String", 34, true])
