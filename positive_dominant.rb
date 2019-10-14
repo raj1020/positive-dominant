11c1560325dd4fda64550c35d2dcf2895deaa9bd
raj1020 <raj.1020@hotmail.com>
2019-10-14 14:08:21 +1100

    added loop to count the number of unique positive and negative integers

--- positive_dominant.rb
+++ positive_dominant.rb
@@ -1,6 +1,27 @@
 def positive_dominant(arr)
     #Your code here
-    
+    i=0
+    positives=0
+    negatives=0
+    while i<arr.count
+        j=i+1
+        while j<arr.count
+            if arr[i]=arr[j]
+                arr.delete_at(j)
+
+                
+            end
+            if arr[i]>0
+                positives+=1
+            end
+
+            if arr[i]<0
+                negatives+=1
+            end
+            j+=1
+        end
+        i+=1
+    end
 
 end
 
