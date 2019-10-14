fa7f6defe40ac35d396e217049d5a66a1d97f60d
raj1020 <raj.1020@hotmail.com>
2019-10-14 16:21:49 +1100

    added another loop to remove non-numbers from array

--- positive_dominant.rb
+++ positive_dominant.rb
@@ -2,18 +2,29 @@ def positive_dominant(arr)
     #Your code here
     i=0
     a=0
-    b=0
     positives=0
     negatives=0
+    while a<arr.count
+        if (arr[a].is_a?(Integer) || arr[a].is_a?(Float))== true
+             
+             a+=1
+        else
+         arr.delete_at(a)
+        end
+     
+     end
     
     while i<(arr.count)
+        
+
+
         j=i+1
         while j<arr.count
             if arr[i]==arr[j]
                 arr.delete_at(j)
                 next
             end
-            p "j=#{j}"
+            
             j+=1
            
         end
@@ -25,7 +36,7 @@ def positive_dominant(arr)
         if arr[i]<0
             negatives+=1
         end
-        p "i=#{i}"
+        
         i+=1
        
     end
@@ -36,6 +47,6 @@ p negatives
 end
 
  positive_dominant([5, 99, -3, -8, 832, -3, -8])
-#p positive_dominant([2, 2, 2, -5, 2, 2, 2, -3])
-#p positive_dominant([2, 4, 6, -6, -4, -2])
-#p positive_dominant(["String", 34, true])
+ positive_dominant([2, 2, 2, -5, 2, 2, 2, -3])
+ positive_dominant([2, 4, 6, -6, -4, -2])
+ positive_dominant(["String", 34, true])
